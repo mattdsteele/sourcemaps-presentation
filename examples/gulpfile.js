e3bd862d04d51a005d5f1b801df865c90ae376b5
Matt Steele <orphum@gmail.com>
2015-02-26 21:50:52 -0600

    Example2, with ES6

--- examples/gulpfile.js
+++ examples/gulpfile.js
@@ -9,7 +9,8 @@ var gulp = require('gulp'),
   autoprefixer = require('gulp-autoprefixer'),
   connect = require('gulp-connect'),
   sass = require('gulp-sass'),
-  sourceMaps = require('gulp-sourcemaps');
+  sourceMaps = require('gulp-sourcemaps'),
+  streamqueue = require('streamqueue');
 
 gulp.task('connect', function() {
   connect.server({
@@ -37,30 +38,41 @@ function css(streams, fileName) {
     .pipe(gulp.dest('dist'))
     .pipe(connect.reload());
 }
-gulp.task('js', function() {
 
+//JS
+var deps = [
+  './node_modules/angular/angular.js',
+  './node_modules/lodash/index.js'
+];
+
+gulp.task('js', function() {
+  //JS
+  var jsFiles = deps.concat(['./src/js/*.js']);
+  var jsStream = gulp.src(jsFiles)
+    .pipe(sourceMaps.init({ loadMaps: true }));
+
+  //Now smoosh them together
+  return js(es.merge(jsStream), 'example1.js');
+});
+
+gulp.task('js2', function() {
+  //JS
+  var jsFiles = deps.concat(['/src/js/*.js']);
+  var jsStream = gulp.src(jsFiles)
+    .pipe(sourceMaps.init({ loadMaps: true }));
+    //
   //ES6
   var es6Stream = gulp.src(['src/es6/*.es6'])
     .pipe(sourceMaps.init({ loadMaps: true}))
     .pipe(babel());
-
-  //JS
-  var deps = [
-    './node_modules/angular/angular.js',
-    './node_modules/lodash/index.js'
-  ];
-  var jsFiles = deps.concat(['./src/js/*.js']);
-  var jsStream = gulp.src(jsFiles)
-    .pipe(sourceMaps.init({ loadMaps: true }));
 
   //TypeScript
 
   //CoffeeScript
 
   //ArnoldC
-
-  //Now smoosh them together
-  return js(es.merge(es6Stream, jsStream), 'example1.js');
+  return js(streamqueue({ objectMode: true }, jsStream, es6Stream), 'example2.js');
+  //return js(es.merge(es6Stream, jsStream), 'example2.js');
 });
 
 gulp.task('css', function() {
@@ -80,8 +92,8 @@ gulp.task('css', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['./src/**/*'], ['js']);
+  gulp.watch(['./src/**/*'], ['js', 'js2']);
   gulp.watch(['src/sass/*', 'src/css/*'], ['css']);
 });
 
-gulp.task('default', ['js', 'css', 'connect', 'watch']);
+gulp.task('default', ['js', 'js2', 'css', 'connect', 'watch']);
--- examples/src/es6/classTest.es6
+++ examples/src/es6/classTest.es6
@@ -1,5 +1,10 @@
-class Test {
-  constructor(name) {
-    console.log(`Thank me for being here, ${name}!`);
+class ES6Controller {
+  constructor($scope) {
+    console.log('This is a scope $scope');
+    this.data = 'What';
   }
 }
+
+angular.module('nejs', [])
+.controller('ES6Controller', ES6Controller);
+
